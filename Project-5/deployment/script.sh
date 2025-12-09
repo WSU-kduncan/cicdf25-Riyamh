8729a241ee636dd6850d581d465cbdead48589df
Riyam Hameed <hameed.10@wright.edu>
2025-12-08 19:39:03 -0500

    Created deployment folder

---
+++ Project-5/deployment/script.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# kill the old container process - stopped & then removed
+docker stop project5
+docker remove project5
+# pull fresh image
+docker pull rhameed/hameed-project3:latest
+# run new container by name, with restart automagic
+docker run -d -p 80:80 --name project5 --restart always rhameed/hameed-project3:latest
